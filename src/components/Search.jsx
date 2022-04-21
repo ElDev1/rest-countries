80206e45f6214bb415b3d893fad88e0253071697
Devi Amaolo <deviamaolo@gmail.com>
2022-04-21 02:50:41 -0300

    creating filter button and logic

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
 import {AiOutlineSearch} from 'react-icons/ai';
 import '../styles/search.css'
-import {useState} from 'react';
+import {useEffect} from 'react';
 
 
 const Search = ({searchCountries, searchInput, setData}) => {
   
+  const region = [
+    {
+      name:"Global"
+    },
+    {
+      name: "Africa",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Oceania",
+    },
+    {
+      name: "Americas",
+    },
+    {
+      name: "Europe",
+    },
+  ]
+
+  const fetchCountryByRegion = async (region) => {
+    if(region !== "Global" && region !== undefined) {
+      console.log(region, "hola")
+      const res = await fetch(
+        `https://restcountries.com/v3.1/region/${region}`
+      )
+      const data = await res.json()
+        setData(data)
+        console.log(data)
+      } else {
+        const res = await fetch(
+          `https://restcountries.com/v3.1/all`
+        )
+        const data = await res.json()
+          setData(data)
+      }
+  }
+
+  useEffect(() => {
+    fetchCountryByRegion()
+  }, [])
+
 
   return (
     <div className='container-fluid d-flex justify-content-around mb-5 mt-5'>
@@ -13,10 +56,10 @@ const Search = ({searchCountries, searchInput, setData}) => {
             <AiOutlineSearch className='search-icon'/>
             <input type="text" onChange={(e) => searchCountries(e.target.value)} value={searchInput} id='search' placeholder='search...'></input>
         </div>
-        <div class="input" placeholder='Filter'>
-          <select class="form-select" id="inputGroupSelect01">
-            <option value="Filter by region">Filter by region</option>
-            <option value="America">America</option>
+        <div className="input" placeholder='Filter'>
+          <select className="form-select" id="select" name="select" value={region.name} onChange={(e) => fetchCountryByRegion(e.target.value)}>
+            <option value="Global">Global</option>
+            <option value="Americas">Americas</option>
             <option value="Europe">Europe</option>
             <option value="Asia">Asia</option>
             <option value="Africa">Africa</option>
